d325f15e2d561abaf3fdacc294c6c78c8ff3992c
zr7yzzy <140452434+zr7yzzy@users.noreply.github.com>
2024-10-18 09:00:13 -0300

    .

--- main.js
+++ main.js
@@ -3,7 +3,15 @@ function criaCartão (categoria, pergunta, reposta) {
     let cartao = document.createElement('article')
     cartao.className = 'cartao';
     cartao.innerHTML = `
-
+    <div class="conteudo-cartao">
+    <h3>${categoria}</h3>
+    <div class="pergunta-cartao">
+        <p>${pergunta}</p>
+    </div>
+    <div class="resposta-cartao">
+        <p>${reposta}</p>
+    </div>
+</div>
     `
     container.appendChild(cartao);
 }

b2544bcdd22b8303142a40f5766c15f98d39a21e
zr7yzzy <140452434+zr7yzzy@users.noreply.github.com>
2024-10-25 12:03:17 -0300

    cvxc

---
+++ pag2.js
@@ -0,0 +1,17 @@
+function criaCartão (categoria, pergunta, reposta) {
+    let container = document.getElementById('container');
+    let cartão = document.createElement('article')
+    cartão.className = 'cartão';
+    cartão.innerHTML = `
+    <div class="conteudo-cartão">
+    <h3>${categoria}</h3>
+    <div class="pergunta-cartão">
+        <p>${pergunta}</p>
+    </div>
+    <div class="resposta-cartão">
+        <p>${reposta}</p>
+    </div>
+</div>
+    `
+    container.appendChild(cartão);
+}
